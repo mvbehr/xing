3f68b9eec6fd99c38dfabea67d09eb02befcdc55
Robin Wenglewski <robin@wenglewski.de>
2013-06-19 19:47:35 +0200

    add fixture_as_json and configure rspec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,6 +5,7 @@ require 'xing'
 require 'rspec'
 require 'webmock/rspec'
 require 'pry'
+require 'json'
 
 def fixture_path
   File.expand_path("../fixtures", __FILE__)
@@ -14,3 +15,18 @@ def fixture(file)
   File.new(fixture_path + '/' + file)
 end
 
+def fixture_as_json(path)
+  absolute_path = File.expand_path path, fixture_path
+  JSON.parse(File.read absolute_path)
+end
+
+
+RSpec.configure do |config|
+  config.treat_symbols_as_metadata_keys_with_true_values = true
+
+  config.filter_run :focus
+  config.run_all_when_everything_filtered                = true
+
+  # make RSpec stop after first failure
+  config.fail_fast                                       = true
+end
